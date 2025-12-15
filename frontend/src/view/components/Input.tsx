bcd2d56ed7ffc5f72346616e588a5d70b7c3bf5a
castro <antoniog_castro@hotmail.com>
2025-12-15 13:06:04 -0400

    fix: primeira versão do componente de input

--- frontend/src/view/components/Input.tsx
+++ frontend/src/view/components/Input.tsx
@@ -1,7 +1,27 @@
-export default function Input(){
-    return (
-    <input type="text" />
+import { ComponentProps, forwardRef } from "react";
 
-
-    );
+interface InputProps extends ComponentProps<"input"> {
+    name?: string;
+    label?: string
+    placeholder?: string;
+    className?: string;
+    type: "password" | "email" | "text" | "stack"
 }
+
+  export const Input = forwardRef< HTMLInputElement, InputProps>( 
+    ({
+        name,
+        label,
+        type,
+        className,
+        placeholder,
+  }) => {
+    return (
+        <div>
+        <label> {label} </label>
+        <input type={type} placeholder={placeholder} className={className} name={name} />
+        </div>
+    )
+  }
+    
+    );
